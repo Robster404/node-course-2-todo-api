9311f0ba7cc4943b8f629d97a3b8729a74b17c1f
Rob <rob@domains.co.za>
2018-06-01 13:31:02 +0200

    Add tests for PATCH /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -141,3 +141,41 @@ describe('DELETE /todos/:id', () => {
 		.end(done);
 	});
 });
+
+describe('PATCH /todos/:id', () => {
+	it('should update the todo', (done) => {
+		var hexId = todos[0]._id.toHexString();
+		var text = 'testing patch 1';
+		request(app)
+			.patch(`/todos/${hexId}`)
+			.send({
+				"completed": true,
+				text
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(text);
+				expect(res.body.todo.completed).toBe(true);
+				expect(res.body.todo.completedAt).toBeA('number');
+			})
+		.end(done);
+	});
+	
+	it('should clear completeAt when todo is not completed', (done) => {
+		var hexId = todos[1]._id.toHexString();
+		var text = 'testing patch 2';
+		request(app)
+			.patch(`/todos/${hexId}`)
+			.send({
+				"completed": false,
+				text
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(text);
+				expect(res.body.todo.completed).toBe(false);
+				expect(res.body.todo.completedAt).toNotExist();
+			})
+		.end(done);
+	});
+});
